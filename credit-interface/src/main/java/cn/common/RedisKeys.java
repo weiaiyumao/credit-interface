a009c62eecd5e6eadc6a5dd7226f8a01f9990bf8
empty <weiaiyumao@163.com>
2017-12-06 23:17:32 +0800

    新增redis 缓存

---
+++ credit-interface/src/main/java/cn/common/RedisKeys.java
@@ -0,0 +1,62 @@
+package main.java.cn.common;
+
+public class RedisKeys {
+	
+	private static RedisKeys rediskeys;
+	
+	public static RedisKeys getInstance() {  
+        if (rediskeys == null) {    
+            synchronized (RedisKeys.class) {    
+               if (rediskeys == null) {    
+            	   rediskeys = new RedisKeys();   
+               }    
+            }    
+        }    
+        return rediskeys;   
+    }  
+	
+	/**
+	 * 获取空号检测 需要检测的总条数key （根据文件获取的总条数）
+	 * @return
+	 */
+	public String getKhTestCountKey(String userId) {
+		return "kh:tc:" + userId;
+	}
+	
+	/**
+	 * 获取空号检测 已经成功检测的总条数（运行中，不考虑不计费的条数）
+	 * @param userId
+	 * @return
+	 */
+	public String getkhSucceedTestCountkey(String userId){
+		return "kh:stc:" + userId;
+	}
+	
+	/**
+	 * 获取空号检测 已经成功检测的总条数（运行结束需要记账的总条数）
+	 * @param userId
+	 * @return
+	 */
+	public String getkhSucceedClearingCountkey(String userId){
+		return "kh:scc:" + userId;
+	}
+	
+	/**
+	 * 获取空号检测 检测方法加锁名称
+	 * @param userId
+	 * @return
+	 */
+	public String getkhTheTestFunKey(String mobile) {
+		return "kh:ttf:" + mobile;
+	}
+	
+	/**
+	 * 获取空号检测 redis锁的唯一标识
+	 * @param userId
+	 * @return
+	 */
+	public String getkhRedisLockIdentifier(String userId) {
+		return "kh:rli:" + userId;
+	}
+	
+}
