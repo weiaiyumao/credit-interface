8bca51d9b4cf8861f56f2938ee5881aeb88fe1eb
empty <weiaiyumao@163.com>
2018-03-06 10:34:42 +0800

    新增系统 code

---
+++ credit-interface/src/main/java/cn/common/SysCode.java
@@ -0,0 +1,17 @@
+package cn.common;
+
+/**
+ * 系统业务配置参数
+ */
+public class SysCode {
+
+    /**
+     * 空号API类型
+     */
+    public static final String kh_api_type = "1";
+
+    /**
+     * 账户二次清洗API类型
+     */
+    public static final String rq_api_type = "2";
+}
